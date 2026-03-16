a3e999167104bfb814a7b338ce8d15d7b0801822
Rahul (Prakrit) <heyrahul.0566@gmail.com>
2026-03-16 12:05:43 +0530

    commit_13

--- sw.js
+++ sw.js
@@ -1,8 +1,11 @@
-const CACHE = 'notiva-v5';
+const CACHE = 'notiva-v6';
 const ASSETS = [
   '/Notiva-Notes/',
   '/Notiva-Notes/index.html',
+  '/Notiva-Notes/mobile.html',
+  '/Notiva-Notes/desktop.html',
   '/Notiva-Notes/manifest.json',
+  'https://fonts.googleapis.com/css2?family=Literata:ital,wght@0,300;0,400;0,500;0,700;1,400&family=JetBrains+Mono:wght@300;400;500&family=Syne:wght@400;600;700;800&display=swap',
   'https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,400&family=Instrument+Serif:ital@0;1&family=JetBrains+Mono:wght@400;500&display=swap'
 ];
 self.addEventListener('install', e => e.waitUntil(
